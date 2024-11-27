b8b8be696deb6f7c5422c45185379a011a11aee4
curtisclan03 <soccershawn03@gmail.com>
2024-11-27 14:57:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+function newp(var1) {
+  document.getElementById("P_homie").style.display = "none"
+  document.getElementById("P_us").style.display = "none"
+  document.getElementById("P_search").style.display = "none"
+  document.getElementById("P_sell").style.display = "none"
+  document.getElementById("P_contact").style.display = "none"
+  document.getElementById("P_BA").style.display = "none"
+  document.getElementById(var1).style.display = "block"
+}
 
+function minimenu() {
+  document.getElementById("MenuDropdown").classList.toggle("showmenu")
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".material-symbols-outlined")) {
+    var dropdowns = document.getElementsByClassName("icon")
+    var i
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i]
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show")
+      }
+    }
+  }
+}
+
+function getvalue() {
+thevalue = document.getElementById('myinput').value
+document.getElementById('valuediv').textContent = 'Results Pending For "' + thevalue + '"'
+}
